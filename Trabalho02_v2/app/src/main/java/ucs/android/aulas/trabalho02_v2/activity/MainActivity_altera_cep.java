8c5086921fee26b5abebaaa89e3bdc671cb05907
Rodrigo Stochero <rsaguiar@ucs.br>
2022-05-30 22:12:30 -0300

    ALTERAÇÃO
    ADICIONA TELA DE ALTERAÇÃO, ACRESCENTADO EDITS

---
+++ Trabalho02_v2/app/src/main/java/ucs/android/aulas/trabalho02_v2/activity/MainActivity_altera_cep.java
@@ -0,0 +1,16 @@
+package ucs.android.aulas.trabalho02_v2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ucs.android.aulas.trabalho02_v2.R;
+
+public class MainActivity_altera_cep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_altera_cep);
+    }
+}
