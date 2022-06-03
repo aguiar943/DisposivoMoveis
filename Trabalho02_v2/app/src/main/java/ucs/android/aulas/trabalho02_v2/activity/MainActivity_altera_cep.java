68da90776779ab07068dec4a43b1274416aff6f1
Rodrigo Stochero <rsaguiar@ucs.br>
2022-06-03 14:53:31 -0300

    ADICIONADO BOTOES
    ADICIONADO BOTOES  E SUAS FUNCOES

--- Trabalho02_v2/app/src/main/java/ucs/android/aulas/trabalho02_v2/activity/MainActivity_altera_cep.java
+++ Trabalho02_v2/app/src/main/java/ucs/android/aulas/trabalho02_v2/activity/MainActivity_altera_cep.java
@@ -2,9 +2,14 @@ package ucs.android.aulas.trabalho02_v2.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import ucs.android.aulas.trabalho02_v2.DAO.BDSQLiteHelper;
 import ucs.android.aulas.trabalho02_v2.R;
@@ -35,5 +40,37 @@ public class MainActivity_altera_cep extends AppCompatActivity {
         bairro.setText(String.valueOf(json.getBairro()));
         uf.setText(String.valueOf(json.getUf()));
         ibge.setText(String.valueOf(json.getIbge()));
+    }
+
+
+    public void AcaoBotao(View view){
+        switch (view.getId()) {
+            case (R.id.BtnSalvar):
+                Toast.makeText(this, "Salvar", Toast.LENGTH_SHORT).show();
+                break;
+            case (R.id.BtnCancelar):
+                finish();
+                Toast.makeText(this, "Cancelar", Toast.LENGTH_SHORT).show();
+                break;
+            case (R.id.BtnRemover):
+                mostraAlerta();
+                break;
+        }
+    }
+
+    private void mostraAlerta() {
+        AlertDialog.Builder ConfirmaItem = new AlertDialog.Builder(MainActivity_altera_cep.this);
+        ConfirmaItem.setTitle("Atenção!");
+        ConfirmaItem.setMessage("Confirma Exclusão do CEP?");
+        ConfirmaItem.setCancelable(false);
+        ConfirmaItem.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which ) {
+                Toast.makeText(MainActivity_altera_cep.this, "CEP removido", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+        ConfirmaItem.setNegativeButton("Não",null);
+        ConfirmaItem.create().show();
     }
 }
